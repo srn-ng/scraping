934dae174b671ff1032c6911b717a51bd6c4bf59
srn-ng <neghouche.serine@gmail.com>
2023-07-26 17:56:16 +0100

    last change

--- khadmoney.py
+++ khadmoney.py
@@ -18,7 +18,7 @@ today_date = datetime.today().strftime('%Y-%m-%d')
 
 # Enregistrer les informations dans un fichier CSV
 with open('KHadmoney.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['Catégorie','URL', 'Titre', 'Prix', 'Date']
+    fieldnames = ['Catégorie','URL', 'Titre', 'Prix', 'Date of scraping']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader() 
@@ -37,10 +37,10 @@ with open('KHadmoney.csv', 'w', newline='', encoding='utf-8') as csvfile:
                 price_element = element.find('span', class_='woocommerce-Price-amount')
                 price = price_element.text.strip()
                 ######################################################################################
-                formations_info.append({  'URL':url ,'Titre': title, 'Prix': price, 'Date': today_date})
+                formations_info.append({  'URL':url ,'Titre': title, 'Prix': price, 'Date of scraping': today_date})
 
     
-            writer.writerow({'Catégorie': f'Catégorie {index}','URL': '', 'Titre': '', 'Prix': '', 'Date': ''})
+            writer.writerow({'Catégorie': f'Catégorie {index}','URL': '', 'Titre': '', 'Prix': '', 'Date of scraping': ''})
 
             for formation in formations_info:
                 formation_with_spaces = {key: f" {value.strip()} " for key, value in formation.items()}
